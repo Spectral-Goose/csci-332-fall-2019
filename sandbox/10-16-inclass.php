167ce107901230cea102d704c0776cd6bcfbc291
Spectral-Goose <taylorsingleton7567@gmail.com>
2019-10-16 14:29:13 -0400

    10/16

---
+++ sandbox/10-16-inclass.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        // This is a comment line
+        echo "Hello world!<br />";
+        Echo "This is another <span style='color: red'>hello world!</span> <br />";
+        $x = 5;
+        $y = "String: ";
+        $z = "This is another string";
+        echo $y . $z;
+    ?>
+</body>
+</html>
