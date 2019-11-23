347485f02a73746cc57cc7ab4d5196e6943126e6
Spectral-Goose <taylorsingleton7567@gmail.com>
2019-11-23 15:26:33 -0500

    kms

---
+++ sandbox/php-mysql-practice/input.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <form action="mysql-queries.php" method="post">
+        First Name: <input type="text" name="first"/><br/>
+        Last Name: <input type="text" name="last"/><br/>
+        Test 1: <input type="text" name="test1"/><br/>
+        Test 2: <input type="text" name="test2"/><br/>
+        Final: <input type="text" name="test3"/><br/>
+        <input type="submit" name="submit" value="submit" />
+    </form>
+</body>
+</html>
--- sandbox/php-mysql-practice/mysql-queries.php
+++ sandbox/php-mysql-practice/mysql-queries.php
@@ -28,14 +28,30 @@
 
         $servername = "localhost";
         $username = "root";
-        $password = "Taydyboo7567!";
+        $password = "Gggoooooossseee777!!!";
         $database = "CSCI332";
         $conn = new mysqli($servername, $username, $password, $database);
 
         if(!$conn) {
             die("Connection failed:" . $conn->connect_error);
         }
-        $sql = "create table student (
+
+        $sql = "insert into students (firstname, lastname, test1, test2, test3) values 
+                ('" . $_POST['first'] . "',
+                 '" . $_POST['last'] . "',
+                 '" . $_POST['test1'] . "',
+                 '" . $_POST['test2'] . "',
+                 '" . $_POST['test3'] . "');";
+        echo $sql . "<br/>";
+        if($conn->query($sql) == true) {
+            echo "inserted successfully";
+        }
+        else {
+            echo "failed to insert: " . $conn->error;
+        }
+
+        // create a table called students
+        /*$sql = "create table student (
             id int(6) unsigned auto_increment primary key,
             firstname varchar(30) not null,
             lastname varchar(30) not null,
@@ -49,7 +65,30 @@
         }
         else {
             echo "Error creating table:" . $conn->error . "\n";
+        }*/
+
+        // create a database of CSCI315 if it doesnt already exist
+        /*$sql = "create database if not exists CSCI315";
+        if($conn->query($sql) == true) {
+            echo "database create successfully";
         }
+        else {
+            echo "database creation failed";
+        }*/
+
+        // create table students in database CSCI332
+        /*$sql = "create table students (ID int(6) unsigned auto_increment primary key, 
+                firstname varchar(30) not null, lastname varchar(30) not 
+                null, test1 int, test2 int, test3 int, grade float(10.2));";
+
+        if($conn->query($sql) == true) {
+            echo "table created successfully";
+        }
+        else {
+            echo "table creation failed";
+        }*/
+
+        // close the connection. necessary at the end of every php script using mysql
         $conn->close();
     ?>
 </body>
